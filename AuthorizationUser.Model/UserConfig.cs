1bb1e206fe98ecc77aea295f15ff4b819d4b5f7b
Starinin Andrey <starininandrey@gmail.com>
2022-07-06 21:10:14 +0300

    Add UserConfig.cs

---
+++ AuthorizationUser.Model/UserConfig.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json;
+
+namespace AuthorizationUser.Model;
+
+public static class UserConfig
+{
+    public static string Path = "user.json";
+    
+    public static void ExportToFile(User user)
+    {
+        using var file = new FileStream(Path, FileMode.OpenOrCreate, FileAccess.Write);
+        JsonSerializer.Serialize(file, user);
+    }
+
+    public static User? ImportFromFile()
+    {
+        using var file = new FileStream(Path, FileMode.Open, FileAccess.Read);
+        return JsonSerializer.Deserialize<User>(file);
+    }
+}
