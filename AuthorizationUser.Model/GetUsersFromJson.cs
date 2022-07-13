b1046c519e7591ca8ecc752715ee57f5f60111e1
Starinin Andrey <starininandrey@gmail.com>
2022-07-13 19:46:10 +0300

    Добавление классов для работы со списком логинов и паролей

---
+++ AuthorizationUser.Model/GetUsersFromJson.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+
+namespace AuthorizationUser.Model;
+
+public static class GetUsersFromJson
+{
+    private const string Path = "users.json";
+
+    public static ListOfLoginAndPassword GetListOfLoginAndPassword()
+    {
+        using var file = new FileStream(Path, FileMode.Open, FileAccess.Read);
+        var users = JsonSerializer.Deserialize<List<User>>(file);
+
+        var dict = users.ToDictionary(user => user.Login, user => user.Password);
+
+        return new ListOfLoginAndPassword(dict);
+    }
+}
---
+++ AuthorizationUser.Model/ListOfLoginAndPassword.cs
@@ -0,0 +1,15 @@
+﻿namespace AuthorizationUser.Model;
+
+public class ListOfLoginAndPassword
+{
+    private readonly Dictionary<string, string> _dictionary;
+
+    public ListOfLoginAndPassword(Dictionary<string, string> dictionary)
+    {
+        _dictionary = dictionary;
+    }
+
+    public bool CheckLogin(string login) => _dictionary.ContainsKey(login);
+
+    public bool CheckPassword(string login, string password) => _dictionary[login] == password;
+}
