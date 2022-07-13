8fa4d6132d990415431f0e3d0f0554fc29c3a699
Starinin Andrey <starininandrey@gmail.com>
2022-07-13 19:21:11 +0300

    Отказ от оповещения об изменнении свойств

--- AuthorizationUser.App/Windows/Main/MainWindow.xaml.cs
+++ AuthorizationUser.App/Windows/Main/MainWindow.xaml.cs
@@ -1,35 +1,15 @@
-﻿using System.ComponentModel;
-using System.Runtime.CompilerServices;
-using System.Windows;
-using AuthorizationUser.App.Annotations;
+﻿using System.Windows;
 using AuthorizationUser.Model;
 
 namespace AuthorizationUser.App.Windows.Main;
-public partial class MainWindow : Window, INotifyPropertyChanged
+public partial class MainWindow : Window
 {
     private User _user;
-    public User User
-    {
-        get => _user;
-        set
-        {
-            _user = value;
-            OnPropertyChanged(nameof(User));
-        }
-    }
-    
+
     public MainWindow()
     {
-        User = new User();
+        _user = new User();
         
         InitializeComponent();
-    }
-
-    public event PropertyChangedEventHandler? PropertyChanged;
-
-    [NotifyPropertyChangedInvocator]
-    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
-    {
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
